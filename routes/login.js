6c5f85bed6262ef45e70ab894e33389385c4beb3
Vaios Laskarelias <vaios.lask@gmail.com>
2019-12-28 23:25:35 +0200

    vasika pramata
    variomoun na kanw ta sql

--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,35 @@
 const express = require('express')
 const login = express.Router()
 const mysql = require('mysql')
-const bodyparser = require('body-parser')
+// const bodyparser = require('body-parser')
 
-login.get('/login', (req, res) => {
-    res.sendFile('sign_in.html', {root: './public/login_forms' })
+login.get('/', (req, res) => {
+    res.sendFile('index.html', { root: 'public/login_forms' }) 
+})
+
+login.get('/sign_in', (req, res) => {
+    res.sendFile('sign_in.html', { root: './public/login_forms' })
 })
 
 login.get('/sign_up', (req, res) => {
-    res.sendFile('sign_up.html', {root: './public/login_forms' })
+    res.sendFile('sign_up.html', { root: './public/login_forms' })
+})
+
+login.post('/sign_up', (req, res) => {
+    username = req.body.username
+    email = req.body.email
+    console.log(username, email)
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '1234',
+        database: 'web'
+    })
+    
+    connection.query("SELECT * FROM users WHERE email = ? ", (err, rows, fields) => {
+        res.send("ekanes sign up")
+    })
 })
 
 module.exports = login
-
--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 const mysql = require('mysql')
-const bodyparser = require('body-parser')
+// const bodyparser = require('body-parser')
 
 app.use(express.static('public'))
 app.use(express.static('public/login_forms'))
+app.use(express.urlencoded({ extended: false }))
 
 const login = require('../routes/login.js')
 const user = require('../routes/user.js')
@@ -13,9 +14,5 @@ const admin = require('../routes/admin.js')
 app.use(login)
 // app.use(user)
 // app.use(admin)
-
-app.get('/', (req, res) => {
-    res.send()
-})
 
 app.listen(3000)
